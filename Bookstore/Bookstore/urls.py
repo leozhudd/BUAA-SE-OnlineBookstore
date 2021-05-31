543df5caef7b22aaa6a88607c5384cdb68268a23
leozhudd <leozhudd@163.com>
2021-05-31 22:22:35 +0800

    ../..

--- Bookstore/Bookstore/urls.py
+++ Bookstore/Bookstore/urls.py
@@ -15,7 +15,10 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from storeApp.views import *
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', index),
+    path('login/', login),
 ]
