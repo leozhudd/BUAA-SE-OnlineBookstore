5bc3e0cde73dce372bf8d9125efd763ae2984bb9
leozhudd <leozhudd@163.com>
2021-05-31 23:09:56 +0800

    register

--- Bookstore/Bookstore/urls.py
+++ Bookstore/Bookstore/urls.py
@@ -21,4 +21,5 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('', index),
     path('login/', login),
+    path('register/', register),
 ]
--- Bookstore/storeApp/views.py
+++ Bookstore/storeApp/views.py
@@ -9,3 +9,20 @@ def index(request):
 
 def login(request):
     return HttpResponse("login!")
+
+
+def register(request):
+    # 如果用户提交了注册表单：
+    if request.method == 'POST':
+        new_username = request.POST.get('username')
+        new_password1 = request.POST.get('password1')
+        new_password2 = request.POST.get('password2')
+
+        # todo 判断用户是否存在
+
+
+
+
+
+def logout(request):
+    return HttpResponse("logout")
