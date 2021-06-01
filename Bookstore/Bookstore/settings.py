3e346172de059b50ba22c073a52d11f67d8615e1
leozhudd <leozhudd@163.com>
2021-06-01 08:29:24 +0800

    优化数据库结构，修复migrate错误

--- Bookstore/Bookstore/settings.py
+++ Bookstore/Bookstore/settings.py
@@ -79,9 +79,9 @@ DATABASES = {
     # todo 修改数据库名称
     'default': {
         'ENGINE': 'django.db.backends.mysql',
-        'NAME': '小型网上书店',
+        'NAME': 'bookstore',
         'USER': 'root',
-        'PASSWORD': 'csapp',
+        'PASSWORD': 'dd64251018',
         'HOST': '127.0.0.1',
         'PORT': 3306
     }
--- Bookstore/storeApp/views.py
+++ Bookstore/storeApp/views.py
@@ -18,10 +18,19 @@ def register(request):
         new_password1 = request.POST.get('password1')
         new_password2 = request.POST.get('password2')
 
-        # todo 判断用户是否存在
-
-
-
+        # 如果用户已经存在
+        if User.objects.filter(username=new_username):
+            return render(request, 'register.html', {'message': '用户已存在！'})
+        if new_password1 != new_password2:
+            return render(request, 'register.html', {'message' '两次密码不一致！'})
+        else:
+            new_user = User()
+            new_user.username = new_username
+            new_user.password = new_password1
+            new_user.sava()
+            return render(request, 'index.html', {'message': '注册成功！'})
+    else:
+        return render(request, 'register.html', {})
 
 
 def logout(request):
